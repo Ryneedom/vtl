35b9d38470926403595a58488f539e964ac3e44a
shellob <shellob@users.noreply.github.com>
2026-04-09 20:08:41 +0300

    Integrate 5 branches into minimal-working: yarovitchuk (audio), bulachev (enum+typos), lachugin (bbcode), ivannikov (telegram), ishatalov (db)
    - Fix precedence bugs in flags_check functions: flags & X == X -> (flags & X) == X
    - Fix function name typo: VTL_pusblication -> VTL_publication
    - Implement text read/write/op from scratch (bulachev version produced garbage)
    - Rewrite VTL_http_client.c to fix scope and const bugs
    - Add FFmpeg pkg-config cmake module
    - Add error codes: VTL_res_kErr, file errors, ffmpeg errors
    - Disable vtl_tests (broken include path)
    - Link curl, cjson, pq, ssl, crypto

--- VTL/VTL_app_result.h
+++ VTL/VTL_app_result.h
@@ -11,15 +11,32 @@ extern "C"
 typedef enum _VTL_AppResult 
 {
     VTL_res_kOk = 0,
-    
-    
+    VTL_res_kErr = -1,
+    VTL_res_kInvalidParamErr = -2,
+    VTL_res_kMemAllocErr = -3,
+    VTL_res_kFileOpenErr = -4,
+    VTL_res_kFileReadErr = -5,
+    VTL_res_kFileWriteErr = -6,
+
     VTL_res_video_fs_r_kMissingFileErr = 1,
     VTL_res_video_fs_r_kFileIsBusyErr,
 
     VTL_res_video_fs_w_kFileIsBusyErr = 10,
 
+    VTL_res_text_io_r_kInvalidArgument = 20,
+    VTL_res_text_ms_w_kOutOfMemory = 30,
 
-    
+    /* добавлено из feature/impl-video-edit (видео-редактор Дмитрия) */
+    VTL_res_ffmpeg_kFormatError,
+    VTL_res_ffmpeg_kStreamError,
+    VTL_res_ffmpeg_kCodecError,
+    VTL_res_ffmpeg_kIOError,
+    VTL_res_ffmpeg_kInitError,
+    VTL_res_ffmpeg_kMemoryError,
+    VTL_res_ffmpeg_kConversionError,
+    VTL_res_kEncoderNeedsMoreFrames,
+    VTL_res_kEncoderFlushComplete,
+
 } VTL_AppResult;
 
 
