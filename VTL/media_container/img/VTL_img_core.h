052187d8d62d36a3dff155ef5e70a7e3d69ccc75
Полев Артем <poleva@inlinegroup-c.ru>
2026-05-19 22:34:26 +0300

    feat(img): обновление модуля обработки изображений

--- VTL/media_container/img/VTL_img_core.h
+++ VTL/media_container/img/VTL_img_core.h
@@ -7,19 +7,24 @@ extern "C"
 #endif
 
 #include <VTL/media_container/img/VTL_img_data.h>
+#include <VTL/VTL_app_result.h>
 
-VTL_ImageContext* VTL_img_context_Init(void);
-void VTL_img_context_Cleanup(VTL_ImageContext* ctx);
+VTL_ImageContext *VTL_img_context_Init(void);
+void VTL_img_context_Cleanup(VTL_ImageContext *ctx);
 
-int VTL_img_LoadImage(const char* input_path, VTL_ImageContext* ctx);
-int VTL_img_SaveImage(const char* output_path, VTL_ImageContext* ctx);
-int VTL_img_ApplyFilter(VTL_ImageContext* ctx, const VTL_ImageFilter* filter);
+VTL_AppResult VTL_img_LoadImage(const char *input_path, VTL_ImageContext *ctx);
+VTL_AppResult VTL_img_SaveImage(const char *output_path, VTL_ImageContext *ctx);
+VTL_AppResult VTL_img_ApplyFilter(VTL_ImageContext *ctx, const VTL_ImageFilter *filter);
 
-int VTL_img_InitGPU(void);
-void VTL_img_CleanupGPU(void);
+/* Обрабатывает группу изображений параллельно. Возвращает первую встреченную
+ * ошибку из задач (kOk если все задачи успешны). */
+VTL_AppResult VTL_img_ProcessBatch(const char **input_paths,
+                                   const VTL_ImageFilter **filters,
+                                   const char **output_paths,
+                                   int count);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // _VTL_IMG_CORE_H 
+#endif /* _VTL_IMG_CORE_H */
